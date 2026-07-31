8add1f686950339e80ca49aea89f9fd12496b215
Akash kushwaha <kushwahavijay1893@gmail.com>
2026-07-31 11:48:04 +0530

    Time: 7 ms (23.24%) | Memory: 16.4 MB (83.45%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    static bool cmp(int a,int b){
+        return to_string(a)+to_string(b)>to_string(b)+to_string(a);
+    }
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),cmp);
+        if(nums[0]==0)return "0";
+        string ans="";
+        for(int x:nums){
+            ans+=to_string(x);
+        }
+        return ans;
+    }
+};
