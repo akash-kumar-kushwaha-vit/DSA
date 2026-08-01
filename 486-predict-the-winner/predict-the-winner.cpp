4504bd5283c13f7b54779fd9819e1d224c3edac7
Akash kushwaha <kushwahavijay1893@gmail.com>
2026-08-01 13:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int f(int i, int j, vector<int>& nums) {
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int takeLeft = nums[i] - f(i + 1, j, nums);
+        int takeRight = nums[j] - f(i, j - 1, nums);
+
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n, vector<int>(n, INT_MIN));
+
+        return f(0, n - 1, nums) >= 0;
+    }
+};
